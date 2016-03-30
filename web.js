dfb47949744ce473c46934eb3ddc746fcfb53359
Swapnil Kumar <swapnilaryan.nitdgp@gmail.com>
2016-03-31 02:30:50 +0530

    Create angular app using Yeoman

---
+++ web.js
@@ -0,0 +1,11 @@
+/**
+ * Created by !!.Swapnil..Aryan.!! on 31-Mar-16.
+ */
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
