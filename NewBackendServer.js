adb2a7336a61a107cf4e0cd3ac15b11db9619ef5
Swapnil Kumar <swapnilaryan.nitdgp@gmail.com>
2016-04-28 01:00:17 +0530

    CORS tested

--- NewBackendServer.js
+++ NewBackendServer.js
@@ -33,6 +33,7 @@ app.use(bodyParser.json());
 // handle CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 // End handling CORS
