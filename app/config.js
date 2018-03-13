3d4e7bcecc7792543643af0ab44271ed04962bee
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-03-14 00:37:34 +0530

    config

--- app/config.js
+++ app/config.js
@@ -14,7 +14,7 @@
 
 var config = {
     theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-    apiUrl: 'http://18.218.55.255:9000/api',
+    apiUrl: 'http://18.218.55.255:9000/api/userClient',
     localImagePath:'http://18.218.55.255',
     imagePath: 'https://image.tmdb.org/t/p',
     theMovieDBURL: 'http://api.themoviedb.org/3/',
