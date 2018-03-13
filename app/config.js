c335020d1ae15ad1fc3d2cf858be385fcd9ca915
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-03-14 00:32:53 +0530

    config changes

--- app/config.js
+++ app/config.js
@@ -3,21 +3,20 @@
  */
 
 //
-var config = {
-  theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-  apiUrlFn: 'http://localhost:9000/api/userClient',
-  localImagePath: 'http://localhost:9002',
-  imagePath: 'https://image.tmdb.org/t/p',
-  theMovieDBURL:'http://api.themoviedb.org/3/',
-  imageSize: '/w154/'
-};
-
-var siteConfig = config;
-
 // var config = {
-//     theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-//     apiUrl: 'http://18.218.55.255:9000/api',
-//     imagePath: 'https://image.tmdb.org/t/p',
-//     theMovieDBURL:'http://api.themoviedb.org/3/',
-//     imageSize: '/w154/'
+//   theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
+//   apiUrlFn: 'http://localhost:9000/api/userClient',
+//   localImagePath: 'http://localhost:9002',
+//   imagePath: 'https://image.tmdb.org/t/p',
+//   theMovieDBURL:'http://api.themoviedb.org/3/',
+//   imageSize: '/w154/'
 // };
+
+var config = {
+    theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
+    apiUrl: 'http://18.218.55.255:9000/api',
+    imagePath: 'https://image.tmdb.org/t/p',
+    theMovieDBURL: 'http://api.themoviedb.org/3/',
+    imageSize: '/w154/'
+};
+var siteConfig = config;
