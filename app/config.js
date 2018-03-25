f168a31f8afb79e8b5d9abb55bb59ee4b7a6359e
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-03-25 20:45:51 +0530

    Config

--- app/config.js
+++ app/config.js
@@ -19,6 +19,7 @@ var config = {
     localImagePath:'http://18.218.55.255',
     imagePath: 'https://image.tmdb.org/t/p',
     theMovieDBURL: 'http://api.themoviedb.org/3/',
-    imageSize: '/w154/'
+    imageSize: '/w154/',
+    baseUrl: 'http://18.218.55.255:9000/api'
 };
 var siteConfig = config;
