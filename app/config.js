132cc6cf8933f33f4afecc9dd3a477ab3ecb225f
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2018-03-25 18:23:11 +0530

    Server config

--- app/config.js
+++ app/config.js
@@ -4,17 +4,18 @@
 
 //
 // var config = {
-//   theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-//   apiUrlFn: 'http://localhost:9000/api/userClient',
-//   localImagePath: 'http://localhost:9002',
-//   imagePath: 'https://image.tmdb.org/t/p',
-//   theMovieDBURL:'http://api.themoviedb.org/3/',
-//   imageSize: '/w154/'
+//     theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
+//     apiUrlFn: 'http://localhost:9000/api/userClient',
+//     localImagePath: 'http://localhost:9002',
+//     imagePath: 'https://image.tmdb.org/t/p',
+//     theMovieDBURL: 'http://api.themoviedb.org/3/',
+//     imageSize: '/w154/',
+//     baseUrl: 'http://localhost:9000/api'
 // };
 
 var config = {
     theMovieDBKey: '2c9306d42037dfb0de0fc3f153819054',
-    apiUrl: 'http://18.218.55.255:9000/api/userClient',
+    apiUrlFn: 'http://18.218.55.255:9000/api/userClient',
     localImagePath:'http://18.218.55.255',
     imagePath: 'https://image.tmdb.org/t/p',
     theMovieDBURL: 'http://api.themoviedb.org/3/',
